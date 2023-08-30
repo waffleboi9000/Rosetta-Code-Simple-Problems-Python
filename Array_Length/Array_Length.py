97bf730e526e92c9191174d4c15856be5a963764
waffleboi9000 <wafflebomb9000@gmail.com>
2023-08-30 16:10:00 -0400

    solved

---
+++ Array_Length/Array_Length.py
@@ -0,0 +1,4 @@
+# defines an array with 3 elements
+array1 = ["element1", "element2", "element3"]
+# prints the count of the number of items in the array.
+print("The array length is: " + str(len(array1)))
---
+++ Array_Length/README.md
@@ -0,0 +1,6 @@
+Project from: https://rosettacode.org/wiki/Array_length
+# Task
+Determine the amount of elements in an array.
+
+
+As an example use an array holding the strings 'apple' and 'orange'.
