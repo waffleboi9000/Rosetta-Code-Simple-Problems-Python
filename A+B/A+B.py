455dbed401aa3aa2acb16e1b56afa977276f0117
waffleboi9000 <wafflebomb9000@gmail.com>
2023-08-30 15:16:54 -0400

    solved

---
+++ A+B/A+B.py
@@ -0,0 +1,4 @@
+valueA = input("Please enter the first value, A: \n")
+valueB = input("Please enter the second value, B: \n")
+total = int(valueA) + int(valueB)
+print ("Your total is: " + str(total))
